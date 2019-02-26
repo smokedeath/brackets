74a717e7f36337bb715175013607e959f6970b45
smoke <smokedeath87@gmail.com>
2019-02-26 14:56:42 +0600

    test is works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    if (str.trim().length===0 || bracketsConfig.length===0) return false;
+    let stek = [];
+    let first = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i].length!==2) {
+            return false;
+        }
+    }
+
+    const arrStr = str.split('');
+    for (let s = 0; s < arrStr.length; s++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (arrStr[s]===bracketsConfig[i][0]) {
+                if (first[bracketsConfig[i][0]] == null) {
+                    stek.push(bracketsConfig[i][0]);
+                    if (bracketsConfig[i][0]===bracketsConfig[i][1]) { first[bracketsConfig[i][0]] = true; }
+                } else {
+                    delete first[bracketsConfig[i][0]];
+                }
+
+            }
+            if (arrStr[s]===bracketsConfig[i][1]) {
+                if (first[bracketsConfig[i][0]] == null) {
+                    if (stek.length===0) return false;
+                    let p = stek.pop();
+                    if (p !== bracketsConfig[i][0]) return false;
+                }
+            }
+        }
+    }
+
+    return stek.length===0;
 }
